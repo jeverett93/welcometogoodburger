fa320bf6358c1254212c0aeba57fa864866df836
Josh Everett <josheverett93@gmail.com>
2020-05-01 18:05:19 -0500

    set up main.handlebars to be used by handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne(["burger_name", "devoured"], [req.body.name, req.body.sleepy], function(result) {
+  burger.insertOne(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -43,6 +43,19 @@ router.put("/api/burgers/:id", function(req, res) {
     }
   );
 });
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 // Export routes for server.js to use.
 module.exports = router;
