f058e25f37c96ae5f89df2b41eaa0b0ee140f66f
Josh Everett <josheverett93@gmail.com>
2020-05-01 18:47:47 -0500

    pages rendering

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-const cat = require("../models/burger.js");
+const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
